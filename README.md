5c9b6b651b8d269aa7435b2d7c7823a0d2c6b09c
agamgn <agamgn@163.com>
2020-07-03 07:42:28 +0800

    ''

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-#模仿喜马拉雅的微信小程序
+### 个体小店商城（微信小程序端）
+
+
+
+### 项目截图
+
+<img width="1400" src="http://git.hiolabs.com/github/all1.jpg"/>
+
+### 功能列表
++ 首页：搜索、Banner、公告、分类Icons、分类商品列表
++ 详情页：加入购物车、立即购买、选择规格
++ 搜索页：排序
++ 分类页：分页加载商品
++ 我的页面：订单（待付款，待发货，待收货），足迹，收货地址
+
+### 项目结构
+```
+├─config     
+│  └─api　
+├─images    
+│  └─icon
+│  └─nav
+├─lib
+│  └─wxParse　　　
+├─pages
+│  ├─app-auth
+│  ├─cart
+│  ├─category
+│  ├─goods
+│  ├─index
+│  ├─order-check
+│  ├─payResult
+│  ├─search
+│  └─ucenter
+│      ├─address
+│      ├─address-detail
+│      ├─express-info
+│      ├─footprint
+│      ├─goods-list
+│      ├─index
+│      ├─order-details
+│      ├─order-list
+│      └─settings
+├─services
+└─utils
+```
+
+
+
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,75 +1,145 @@
-//index.js
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+const user = require('../../services/user.js');
+
 //获取应用实例
 const app = getApp()
-var utils = require('../../utils/utils.js');
 
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    imgList:[
-      '/image/ad1.jpg',
-      '/image/ad2.jpg',
-      '/image/ad3.jpg',
-      '/image/ad4.jpg',
-      '/image/ad5.jpg',
-      '/image/ad6.jpg',
-      '/image/ad7.jpg'
-    ],
-    navList:[
-      {icon:'/image/nav-icon/diantai.png',events:'goToBangDan',text:'榜单'},
-      {icon:'/image/nav-icon/diantai.png',events:'goToBangDan',text:'听小说'},
-      {icon:'/image/nav-icon/diantai.png',events:'goToBangDan',text:'情感电台'},
-      {icon:'/image/nav-icon/diantai.png',events:'goToBangDan',text:'听知识'},
-      
-    ],
-    swiperCurrent: 0,
-  },
-  //轮播图改变事件
-  swiperChange: function(e){
-    this.setData({
-     swiperCurrent: e.detail.current
-    })
-   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that = this;
-    var url = 'http://mobile.ximalaya.com/mobile/discovery/v3/recommend/hotAndGuess?code=43_310000_3100&device=android&version=5.4.45';
-
-    // 调用的是自己封装的工具函数，在utils中
-    utils.myRequest({
-      url:url,
-      methods:'GET',
-      success:function(result){
-        that.setData({
-          showitem:true,
-          guess:result.data.paidArea.list,
-          xiaoshuocontent:result.data.hotRecommends.list[0].list,
-          xiangshengcontent:result.data.hotRecommends.list[2].list,
-          tuokocontent:result.data.hotRecommends.list[4].list
+    data: {
+        floorGoods: [],
+        openAttr: false,
+        showChannel: 0,
+        showBanner: 0,
+        showBannerImg: 0,
+        goodsCount: 0,
+        banner: [],
+        index_banner_img: 0,
+        userInfo: {},
+        imgurl: '',
+        sysHeight: 0,
+        loading: 0,
+        autoplay:true
+    },
+    onHide:function(){
+        this.setData({
+            autoplay:false
         })
-      },
-      fail:function(){
-        that.setData({
-          showitem:false
+    },
+    goSearch: function () {
+        wx.navigateTo({
+            url: '/pages/search/search',
         })
-      }
-    });
-  },
-  goToBangDan:function(){
-    wx.navigateTo({
-      url: '/pages/index/bangdan/bangdan',
-    })
-  },
-  gotoDetails(e){
-    var url = e.currentTarget.dataset.coverimg;
-    var title = e.currentTarget.dataset.title;
-    wx.navigateTo({
-      url: '/pages/details/details?url='+url+'&title='+title,
-    })
-  }
+    },
+    goCategory: function (e) {
+        let id = e.currentTarget.dataset.cateid;
+        wx.setStorageSync('categoryId', id);
+        wx.switchTab({
+            url: '/pages/category/index',
+        })
+    },
+    getCatalog: function () {
+        let that = this;
+        util.request(api.GoodsCount).then(function (res) {
+            that.setData({
+                goodsCount: res.data.goodsCount
+            });
+        });
+    },
+    handleTap: function (event) {
+        //阻止冒泡 
+    },
+    onShareAppMessage: function () {
+        let info = wx.getStorageSync('userInfo');
+        return {
+            title: '海风小店',
+            desc: '开源微信小程序商城',
+            path: '/pages/index/index?id=' + info.id
+        }
+    },
+    toDetailsTap: function () {
+        wx.navigateTo({
+            url: '/pages/goods-details/index',
+        });
+    },
+    getIndexData: function () {
+        let that = this;
+        util.request(api.IndexUrl).then(function (res) {
+            if (res.errno === 0) {
+                that.setData({
+                    floorGoods: res.data.categoryList,
+                    banner: res.data.banner,
+                    channel: res.data.channel,
+                    notice: res.data.notice,
+                    loading: 1,
+                });
+            }
+        });
+    },
+    onLoad: function (options) {
+        let systemInfo = wx.getStorageSync('systemInfo');
+        var scene = decodeURIComponent(options.scene);
+        this.getCatalog();
+    },
+    onShow: function () {
+        this.getCartNum();
+        this.getChannelShowInfo();
+        this.getIndexData();
+        var that = this;
+        let userInfo = wx.getStorageSync('userInfo');
+        if (userInfo != '') {
+            that.setData({
+                userInfo: userInfo,
+            });
+        };
+        let info = wx.getSystemInfoSync();
+        let sysHeight = info.windowHeight - 100;
+        this.setData({
+            sysHeight: sysHeight,
+            autoplay:true
+        });
+        wx.removeStorageSync('categoryId');
+    },
+    getCartNum: function () {
+        util.request(api.CartGoodsCount).then(function (res) {
+            if (res.errno === 0) {
+                let cartGoodsCount = '';
+                if (res.data.cartTotal.goodsCount == 0) {
+                    wx.removeTabBarBadge({
+                        index: 2,
+                    })
+                } else {
+                    cartGoodsCount = res.data.cartTotal.goodsCount + '';
+                    wx.setTabBarBadge({
+                        index: 2,
+                        text: cartGoodsCount
+                    })
+                }
+            }
+        });
+    },
+    getChannelShowInfo: function (e) {
+        let that = this;
+        util.request(api.ShowSettings).then(function (res) {
+            if (res.errno === 0) {
+                let show_channel = res.data.channel;
+                let show_banner = res.data.banner;
+                let show_notice = res.data.notice;
+                let index_banner_img = res.data.index_banner_img;
+                that.setData({
+                    show_channel: show_channel,
+                    show_banner: show_banner,
+                    show_notice: show_notice,
+                    index_banner_img: index_banner_img
+                });
+            }
+        });
+    },
+    onPullDownRefresh: function () {
+        wx.showNavigationBarLoading()
+        this.getIndexData();
+        this.getChannelShowInfo();
+        wx.hideNavigationBarLoading() //完成停止加载
+        wx.stopPullDownRefresh() //停止下拉刷新
+    },
 })
