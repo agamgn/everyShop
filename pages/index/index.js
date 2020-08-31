12fcde25cedcc2affb11f608edc0311d6699591c
agamgn <agamgn@163.com>
2020-08-31 21:51:50 +0800

    init

--- pages/index/index.js
+++ pages/index/index.js
@@ -52,8 +52,8 @@ Page({
     onShareAppMessage: function () {
         let info = wx.getStorageSync('userInfo');
         return {
-            title: '海风小店',
-            desc: '开源微信小程序商城',
+            title: '个人小店',
+            desc: '个人微信小程序商城',
             path: '/pages/index/index?id=' + info.id
         }
     },
