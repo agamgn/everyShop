e1d0f2ff7f747340c3479ccdd9bfe531730e3fcc
agamgn <agamgn@163.com>
2020-08-31 22:14:25 +0800

    init

---
+++ pages/about/about.js
@@ -0,0 +1,46 @@
+// about.js
+var app = getApp()
+var util = require("../../utils/util.js");
+
+
+var api = require("../../config/api.js");
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    load_statue: true,
+    shopInfo: {
+      name: '个人商城',
+      latitude: 27.4871724214,
+      longitude: 110.8950504844,
+    },
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  showLocation: function (e) {
+    var that = this
+    wx.openLocation({
+      latitude: that.data.shopInfo.latitude,
+      longitude: that.data.shopInfo.longitude,
+      name: that.data.shopInfo.name,
+      address: that.data.shopInfo.address,
+    })
+  },
+  callPhone: function (e) {
+    var that = this
+    wx.makePhoneCall({
+      phoneNumber: that.data.shopInfo.linkPhone,
+    })
+  },
+  reLoad: function (e) {
+    this.loadShopInfo();
+  }
+})
